1779085ff72224150886afbb5ccc0993404351d6
sujeong165 park <sujr110026@gmail.com>
2024-05-27 07:25:05 +0000

    5차 뭔가 이상함....

--- script.js
+++ script.js
@@ -8,6 +8,8 @@ function selectAnswer(answer, nextPage) {
         localStorage.setItem('q1', answer);
     } else if (currentQuestion.endsWith('question2.html')) {
         localStorage.setItem('q2', answer);
+    } else if (currentQuestion.endsWith('question3.html')) {
+        localStorage.setItem('q3', answer);
     }
     window.location.href = nextPage;
 }
@@ -16,16 +18,17 @@ window.onload = function() {
     if (window.location.pathname.endsWith('result.html')) {
         const q1 = localStorage.getItem('q1');
         const q2 = localStorage.getItem('q2');
+        const q3 = localStorage.getItem('q3');
         let result = '';
 
-        if (q1 === '새로운 사람들과 친해지려고 노력한다.' && q2 === '마감 시간에 맞춰서 준비한다.') {
+        if (q1 === '새로운 사람들과 친해지려고 노력한다.' && q2 === '마감 시간에 맞춰서 준비한다.' && q3 === '늘 먹던 대로 무난한 빵') {
             result = '당신은 베이글입니다!';
-        } else if (q1 === '새로운 사람들과 친해지려고 노력한다.' && q2 === '아무 때나 하면 어때') {
+        } else if (q1 === '새로운 사람들과 친해지려고 노력한다.' && q2 === '아무 때나 하면 어때' && q3 === '늘 먹던 대로 무난한 빵') {
             result = '당신은 사워도우입니다!';
-        } else if (q1 === '아는 사람과 깊게 친해지려고 노력한다.' && q2 === '마감 시간에 맞춰서 준비한다.') {
+        } else if (q1 === '아는 사람과 깊게 친해지려고 노력한다.' && q2 === '마감 시간에 맞춰서 준비한다.' && q3 === '늘 먹던 대로 무난한 빵') {
             result = '당신은 바게트입니다!';
-        } else if (q1 === '아는 사람과 깊게 친해지려고 노력한다.' && q2 === '아무 때나 하면 어때') {
-            result = '당신은 바게트입니다!';
+        } else if (q1 === '아는 사람과 깊게 친해지려고 노력한다.' && q2 === '아무 때나 하면 어때' && q3 === '늘 먹던 대로 무난한 빵') {
+            result = '당신은 마늘빵입니다!';
         }
 
         document.getElementById('result').innerText = result;
