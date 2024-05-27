34e6a805f30167e59bbdf6bfc003cdb04d2a577e
sujeong165 park <sujr110026@gmail.com>
2024-05-27 07:16:42 +0000

    4.5차 수정

--- script.js
+++ script.js
@@ -18,14 +18,14 @@ window.onload = function() {
         const q2 = localStorage.getItem('q2');
         let result = '';
 
-        if (q1 === '아침' && q2 === '빨강') {
+        if (q1 === '새로운 사람들과 친해지려고 노력한다.' && q2 === '마감 시간에 맞춰서 준비한다.') {
             result = '당신은 베이글입니다!';
-        } else if (q1 === '점심' && q2 === '초록') {
+        } else if (q1 === '새로운 사람들과 친해지려고 노력한다.' && q2 === '아무 때나 하면 어때') {
             result = '당신은 사워도우입니다!';
-        } else if (q1 === '저녁' && q2 === '파랑') {
+        } else if (q1 === '아는 사람과 깊게 친해지려고 노력한다.' && q2 === '마감 시간에 맞춰서 준비한다.') {
             result = '당신은 바게트입니다!';
-        } else {
-            result = '당신은 크루아상입니다!';
+        } else if (q1 === '아는 사람과 깊게 친해지려고 노력한다.' && q2 === '아무 때나 하면 어때') {
+            result = '당신은 바게트입니다!';
         }
 
         document.getElementById('result').innerText = result;
