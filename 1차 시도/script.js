231a85eedc54b178585d7264cc2c184247284e69
sujeong165 park <sujr110026@gmail.com>
2024-06-03 06:13:00 +0000

    6.3 1차 수정

--- 1차 시도/script.js
+++ 1차 시도/script.js
@@ -2,6 +2,7 @@ function startQuiz() {
     window.location.href = 'question1.html';
 }
 
+
 function selectAnswer(answer, nextPage) {
     const currentQuestion = window.location.pathname;
     if (currentQuestion.endsWith('question1.html')) {
@@ -10,6 +11,24 @@ function selectAnswer(answer, nextPage) {
         localStorage.setItem('q2', answer);
     } else if (currentQuestion.endsWith('question3.html')) {
         localStorage.setItem('q3', answer);
+    } else if (currentQuestion.endsWith('question4.html')) {
+        localStorage.setItem('q4', answer);
+    } else if (currentQuestion.endsWith('question5.html')) {
+        localStorage.setItem('q5', answer);
+    } else if (currentQuestion.endsWith('question6.html')) {
+        localStorage.setItem('q6', answer);
+    } else if (currentQuestion.endsWith('question7.html')) {
+        localStorage.setItem('q7', answer);
+    } else if (currentQuestion.endsWith('question8.html')) {
+        localStorage.setItem('q8', answer);
+    } else if (currentQuestion.endsWith('question9.html')) {
+        localStorage.setItem('q9', answer);
+    } else if (currentQuestion.endsWith('question10.html')) {
+        localStorage.setItem('q10', answer);
+    } else if (currentQuestion.endsWith('question11.html')) {
+        localStorage.setItem('q11', answer);
+    } else if (currentQuestion.endsWith('question12.html')) {
+        localStorage.setItem('12', answer);
     }
     window.location.href = nextPage;
 }
@@ -19,6 +38,15 @@ window.onload = function() {
         const q1 = localStorage.getItem('q1');
         const q2 = localStorage.getItem('q2');
         const q3 = localStorage.getItem('q3');
+        const q3 = localStorage.getItem('q4');
+        const q3 = localStorage.getItem('q5');
+        const q3 = localStorage.getItem('q6');
+        const q3 = localStorage.getItem('q7');
+        const q3 = localStorage.getItem('q8');
+        const q3 = localStorage.getItem('q9');
+        const q3 = localStorage.getItem('q10');
+        const q3 = localStorage.getItem('q11');
+        const q3 = localStorage.getItem('q12');
         let result = '';
 
         if (q1 === '새로운 사람들과 친해지려고 노력한다.' && q2 === '마감 시간에 맞춰서 준비한다.' && q3 === '늘 먹던 대로 무난한 빵') {
