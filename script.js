7ca6499b7bed784e60bb60d2e69368fa103764be
sujeong165 park <sujr110026@gmail.com>
2024-06-03 06:18:52 +0000

    6.3 2차 수정

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
+console.log("Script loaded");
+
 function startQuiz() {
+    console.log("Starting quiz");
     window.location.href = 'question1.html';
 }
 
@@ -28,7 +31,7 @@ function selectAnswer(answer, nextPage) {
     } else if (currentQuestion.endsWith('question11.html')) {
         localStorage.setItem('q11', answer);
     } else if (currentQuestion.endsWith('question12.html')) {
-        localStorage.setItem('12', answer);
+        localStorage.setItem('q12', answer);  // 수정된 부분
     }
     window.location.href = nextPage;
 }
@@ -38,15 +41,16 @@ window.onload = function() {
         const q1 = localStorage.getItem('q1');
         const q2 = localStorage.getItem('q2');
         const q3 = localStorage.getItem('q3');
-        const q3 = localStorage.getItem('q4');
-        const q3 = localStorage.getItem('q5');
-        const q3 = localStorage.getItem('q6');
-        const q3 = localStorage.getItem('q7');
-        const q3 = localStorage.getItem('q8');
-        const q3 = localStorage.getItem('q9');
-        const q3 = localStorage.getItem('q10');
-        const q3 = localStorage.getItem('q11');
-        const q3 = localStorage.getItem('q12');
+        const q4 = localStorage.getItem('q4');
+        const q5 = localStorage.getItem('q5');
+        const q6 = localStorage.getItem('q6');
+        const q7 = localStorage.getItem('q7');
+        const q8 = localStorage.getItem('q8');
+        const q9 = localStorage.getItem('q9');
+        const q10 = localStorage.getItem('q10');
+        const q11 = localStorage.getItem('q11');
+        const q12 = localStorage.getItem('q12');
+
         let result = '';
 
         if (q1 === '새로운 사람들과 친해지려고 노력한다.' && q2 === '마감 시간에 맞춰서 준비한다.' && q3 === '늘 먹던 대로 무난한 빵') {
